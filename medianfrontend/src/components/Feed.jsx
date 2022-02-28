6011904cc22dbe7bfd2f9e4cdf6bef1f87cc7664
adhikari-dikshant <69414612+adhikari-dikshant@users.noreply.github.com>
2022-02-28 14:41:51 +0530

    front end

--- medianfrontend/src/components/Feed.jsx
+++ medianfrontend/src/components/Feed.jsx
@@ -34,6 +34,13 @@ const Feed = () => {
       <Spinner message={`We are adding ${ideaName} ideas to your feed!`} />
     );
   }
+  if (!pins?.length) {
+      return(
+        <h2>
+          No Pins Available
+        </h2>
+      )
+  }
   return (
     <div>
       {pins && (
